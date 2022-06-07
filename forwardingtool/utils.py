33fe8223192eae1103c533c84cd1badac85b009a
Michael Fiederer <michael.fiederer@kontron.com>
2022-06-07 07:12:17 +0200

    Added installation section to readme, fixed get_datafile for frozen versions

--- forwardingtool/utils.py
+++ forwardingtool/utils.py
@@ -8,9 +8,9 @@ def get_datafile(filename: str) -> str:
     Get the filename for a data file within in module.
     """
 
-    if getattr(sys, "frozen", False):
-        # The application is frozen with cx_Freeze
-        directory: str = os.path.dirname(sys.executable)
-    else:
-        directory: str = os.path.dirname(__file__)
+    # if getattr(sys, "frozen", False):
+    #     # The application is frozen with cx_Freeze
+    #     directory: str = os.path.dirname(sys.executable)
+    # else:
+    directory: str = os.path.dirname(__file__)
     return os.path.join(directory, filename)
---
+++ freeze.py
@@ -0,0 +1,54 @@
+"""
+cx_Freeze build script for the ForwardingTool to create a frozen (compiled) version of the tool.
+"""
+
+
+import sys
+import subprocess
+import os.path
+
+from cx_Freeze import setup, Executable
+
+import forwardingtool
+
+
+def create_versionmodule(packagepath: str):
+    """
+    creates a file packagepath/_version.py which defines a __version__ variable
+    which contains the stripped output of "git describe --dirty --always --tags"
+    """
+
+    version = subprocess.run(['git', 'describe', '--dirty', '--always', '--tags'],
+                             capture_output=True, encoding='utf-8').stdout.strip()
+    with open(os.path.join(packagepath, '_version.py'), mode='w', encoding='utf-8') as file:
+        file.write(f'__version__: str = {version!r}\n')
+
+
+buildOptions = {
+    'packages': [
+        "tkinter",
+        "forwardingtool",
+        "keyring",
+        "paramiko",
+        "sshtunnel",
+    ],
+    'excludes': [
+    ],
+    'include_files': [
+        'lastused.json',
+        'README.md',
+    ],
+    'include_msvcr': True
+}
+
+base = 'Win32GUI' if sys.platform == 'win32' else None
+create_versionmodule(packagepath=os.path.dirname(forwardingtool.__file__))
+
+executables = [
+    Executable('main.py', base=base, target_name="ForwardingTool.exe", icon='forwardingtool/logo.ico'),
+]
+
+setup(name='forwardingtool',
+      options={'build_exe': buildOptions},
+      executables=executables,
+      )
