71d4f0aa099fdaec43e6b5b005ff56692e396ea2
slarag <58182961+Slarag@users.noreply.github.com>
2022-09-04 18:48:52 +0200

    Small code cleanup

--- forwardingtool/__init__.py
+++ forwardingtool/__init__.py
@@ -2,9 +2,6 @@
 
 import sys as _sys
 
-__version__ = 'UNKNOWN'
-if getattr(_sys, "frozen", False):
-    try:
-        from forwardingtool._version import __version__
-    except ModuleNotFoundError:
-        pass
+from .views import App
+
+
